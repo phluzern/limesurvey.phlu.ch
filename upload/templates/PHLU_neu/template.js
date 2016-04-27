b79161e1962d881f3232757b055f2b83226ac73b
Adrian Felder <adrian.felder@phlu.ch>
2016-04-27 08:28:39 +0200

    added phlu templates inside upload folder to git

---
+++ upload/templates/PHLU_neu/template.js
@@ -0,0 +1,59 @@
+/*
+ * LimeSurvey
+ * Copyright (C) 2007 The LimeSurvey Project Team / Carsten Schmitz
+ * All rights reserved.
+ * License: GNU/GPL License v2 or later, see LICENSE.php
+ * LimeSurvey is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ * 
+ * 
+ * Description: Javascript file for templates. Put JS-functions for your template here.
+ *  
+ * 
+ * $Id:$
+ */
+
+
+/*
+ * The function focusFirst puts the Focus on the first non-hidden element in the Survey. 
+ * 
+ * Normally this is the first input field (the first answer).
+ */
+function focusFirst(Event)
+{
+	$('#limesurvey :input:visible:enabled:first').focus();
+}
+/*
+ * The focusFirst function is added to the eventlistener, when the page is loaded.
+ * 
+ * This can be used to start other functions on pageload as well. Just put it inside the 'ready' function block
+ */
+
+/* Uncomment below if you want to use the focusFirst function */
+/*
+$(document).ready(function(){
+	focusFirst();
+});
+*/
+
+$(document).ready(function(){
+
+});
+
+
+WebFontConfig = {
+    google: { families: [ 'Open+Sans::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+
